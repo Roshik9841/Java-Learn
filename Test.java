89d3a435e1edc1800c10dbfd4c6d2e3228150d16
I am Roshik <Roshik9841@gmail.com>
2025-08-22 17:23:15 +0545

    Class and obj:

--- Test.java
+++ Test.java
@@ -1,9 +1,15 @@
+class Calculator{
+    int num1,num2;
+    public void add(int num1,int num2){
+        System.out.println("Sum is "+(num1+num2));
+    }
+}
+
 class Program{
     public static void main(String args[]){
-          int age = 25;
-           double height = 5.9;
-           String name = "Alex";
-           System.out.println("Name:"+name+", Age:"+age+", Height:"+height);
+        Calculator c1 = new Calculator();
+        c1.add(10,20);
+        
        
     }
 }
