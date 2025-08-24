a6493ea48fd9d4295132713f4296c816bf482566
I am Roshik <Roshik9841@gmail.com>
2025-08-24 18:57:13 +0545

    enums

--- Test.java
+++ Test.java
@@ -1,15 +1,15 @@
-class Calculator{
-    int num1,num2;
-    public void add(int num1,int num2){
-        System.out.println("Sum is "+(num1+num2));
-    }
+enum Weekdays{
+    sunday,
+    monday,
+    tuesday,
+    wednesday,
+    thursday
 }
 
 class Program{
     public static void main(String args[]){
-        Calculator c1 = new Calculator();
-        c1.add(10,20);
-        
+       Weekdays w1 = Weekdays.sunday;
+       System.out.println(w1);
        
     }
 }
