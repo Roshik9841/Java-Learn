4366d08d9aa4b44fbe4c785479e47b3a0484c565
I am Roshik <Roshik9841@gmail.com>
2025-08-24 19:01:53 +0545

    overriding

--- Test.java
+++ Test.java
@@ -1,15 +1,21 @@
-enum Weekdays{
-    sunday,
-    monday,
-    tuesday,
-    wednesday,
-    thursday
+class Calculator{
+    int num1,num2;
+    public void add(){
+        System.out.println(("ANIMAl BARKS"));
+    }
+}
+class abc extends Calculator{
+    @Override
+    public  void add(){
+    System.out.println("Dog barks");
+    }
 }
 
 class Program{
     public static void main(String args[]){
-       Weekdays w1 = Weekdays.sunday;
-       System.out.println(w1);
+      Calculator c1 = new abc();
+      c1.add();
+        
        
     }
 }
