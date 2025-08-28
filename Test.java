d8b0660abd5ee2517d8f314cf1cda670e2c87e46
I am Roshik <Roshik9841@gmail.com>
2025-08-28 16:29:33 +0545

    With super

--- Test.java
+++ Test.java
@@ -1,19 +1,23 @@
-import java.util.*;
-class Mobile{
-    String brand;
-    int price;
-    static String name;
-
-    public static void show(Mobile m){
-        System.out.println("Brand"+ m.brand + "Price"+ m.price + "Name"+ name);
+class A{
+    public A(){
+        System.out.println("in A");
+    }
+    public A(int n){
+        System.out.println("In A with n");
+    }
+}
+class B extends A{
+    public B(){
+        System.out.println("in B");
+    }
+    public B(int n){
+        super(n);
+        System.out.println("In B int");
     }
 }
 class Program{
     public static void main(String args[]){
-        Mobile m1 = new Mobile();
-        m1.brand = "Samsung";
-        m1.price= 123123;
-        Mobile.name = "Roshik";
-       Mobile.show(m1);
+        B b1 = new B();
+        B b2 = new B(12);
     }
 }
