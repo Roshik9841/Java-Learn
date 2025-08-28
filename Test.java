f9a5582e6457fe64da1e35f664144bbbf93f21d6
I am Roshik <Roshik9841@gmail.com>
2025-08-28 15:37:43 +0545

    Static void and variable

--- Test.java
+++ Test.java
@@ -1,14 +1,19 @@
 import java.util.*;
+class Mobile{
+    String brand;
+    int price;
+    static String name;
 
+    public static void show(Mobile m){
+        System.out.println("Brand"+ m.brand + "Price"+ m.price + "Name"+ name);
+    }
+}
 class Program{
     public static void main(String args[]){
-        StringBuffer sb = new StringBuffer("Hello");
-        System.out.println(sb.capacity());
-        sb.append(" Roshik Maharjan");
-
-        System.out.println(sb);
-
-        sb.deleteCharAt(3);
-        System.out.println(sb);
+        Mobile m1 = new Mobile();
+        m1.brand = "Samsung";
+        m1.price= 123123;
+        Mobile.name = "Roshik";
+       Mobile.show(m1);
     }
 }
